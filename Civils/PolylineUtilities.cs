a16ab9c1d745e16775cfd27f7c8333054abf17f1
MichaelLiddiard <michael.liddiard@jppuk.net>
2017-09-03 21:43:18 +0100

    Quick script for Site Plan
    Quick script created to handle the specila job. Polyline get converted to feature lines, levels read, added 150 to highest and then added text.

--- Civils/PolylineUtilities.cs
+++ Civils/PolylineUtilities.cs
@@ -3,12 +3,18 @@ using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil.ApplicationServices;
 using JPPCommands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using CivSurface = Autodesk.Civil.DatabaseServices.Surface;
+using Entity = Autodesk.AutoCAD.DatabaseServices.Entity;
+using DBObject = Autodesk.AutoCAD.DatabaseServices.DBObject;
+using Autodesk.Civil.DatabaseServices;
 
 [assembly: CommandClass(typeof(JPP.Civils.PolylineUtilities))]
 
@@ -148,6 +154,94 @@ namespace JPP.Civils
                     }
 
                     tr.Commit();
+                }
+            }
+        }
+
+        [CommandMethod("PlineToPlots")]
+        public void PlineToPlots()
+        {
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            PromptSelectionOptions pso = new PromptSelectionOptions();
+            pso.SingleOnly = true;
+            pso.RejectObjectsOnLockedLayers = true;
+            PromptSelectionResult psr = acDoc.Editor.GetSelection(pso);
+            if (psr.Status == PromptStatus.OK)
+            {
+                using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+                {
+                    CivSurface oSurface = null;
+
+                    //Get the target surface
+                    ObjectIdCollection SurfaceIds = CivilApplication.ActiveDocument.GetSurfaceIds();
+                    foreach (ObjectId surfaceId in SurfaceIds)
+                    {
+                        CivSurface temp = surfaceId.GetObject(OpenMode.ForRead) as CivSurface;
+                        if (temp.Name == Civils.Constants.ProposedGroundName)
+                        {
+                            oSurface = temp;
+                        }
+
+                        int plotCount = 0;
+
+                        foreach (SelectedObject so in psr.Value)
+                        {
+                            DBObject obj = acTrans.GetObject(so.ObjectId, OpenMode.ForWrite);
+
+                            if (obj is Curve)
+                            {
+                                plotCount++;
+
+                                //Polyline acPline = obj as Polyline;
+
+                                //Need to add the temp line to create feature line from it
+                                BlockTable acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId, OpenMode.ForRead) as BlockTable;
+                                BlockTableRecord acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                                ObjectId perimId = FeatureLine.Create("plot" + plotCount, obj.ObjectId);
+
+                                FeatureLine perim = acTrans.GetObject(perimId, OpenMode.ForWrite) as FeatureLine;
+                                perim.AssignElevationsFromSurface(oSurface.Id, false);
+
+                                double FinishedFloorLevel = Math.Round(perim.MaxElevation * 1000) / 1000 + 0.15;
+
+                                //Ad the FFL Label
+                                // Create a multiline text object
+                                using (MText acMText = new MText())
+                                {
+                                    Solid3d Solid = new Solid3d();
+                                    DBObjectCollection coll = new DBObjectCollection();
+                                    coll.Add(obj);
+                                    Solid.Extrude(((Region)Region.CreateFromCurves(coll)[0]), 1, 0);
+                                    Point3d centroid = new Point3d(Solid.MassProperties.Centroid.X, Solid.MassProperties.Centroid.Y, 0);
+                                    Solid.Dispose();
+
+                                    acMText.Location = centroid;
+                                    acMText.Contents = FinishedFloorLevel.ToString("F3");
+                                    //acMText.Rotation = Rotation;
+                                    acMText.Height = 7;
+                                    acMText.Attachment = AttachmentPoint.MiddleCenter;
+
+                                    acBlkTblRec.AppendEntity(acMText);
+                                    acTrans.AddNewlyCreatedDBObject(acMText, true);
+                                }
+
+                                //perim.Erase();
+                                obj.Erase();
+                            }
+                            else
+                            {
+                                acDoc.Editor.WriteMessage("Object is not a polyline\n");
+                            }
+
+                        }
+                        
+                        
+                    }
+
+                    acTrans.Commit();
                 }
             }
         }
